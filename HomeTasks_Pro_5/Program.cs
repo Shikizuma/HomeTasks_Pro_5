ba1be227d98eb5b6ba7952ae2d961f3fbabac632
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-23 20:19:46 +0300

    Add project files.

---
+++ HomeTasks_Pro_5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml;
+
+namespace HomeTasks_Pro_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "test.xml"; 
+
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(filePath);
+                DisplayXmlInfo(xmlDoc.DocumentElement!);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+        }
+
+        static void DisplayXmlInfo(XmlNode node)
+        {
+            if (node is XmlElement)
+            {
+                Console.WriteLine("Елемент: " + node.Name);
+
+                if (node.Attributes != null)
+                {
+                    foreach (XmlAttribute attr in node.Attributes)
+                    {
+                        Console.WriteLine("  Атрибут: " + attr.Name + " = " + attr.Value);
+                    }
+                }
+            }
+            else if (node is XmlText)
+            {
+                string text = ((XmlText)node).InnerText.Trim();
+                if (!string.IsNullOrEmpty(text))
+                {
+                    Console.WriteLine("  Текст: " + text);
+                }
+            }
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                DisplayXmlInfo(childNode);
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayAttribute("TelephoneBook.xml");
+
+            //string filePath = "TelephoneBook.xml";
+
+            //try
+            //{
+            //    XmlDocument xmlDoc = new XmlDocument();
+            //    xmlDoc.Load(filePath);
+            //    DisplayAttribute(xmlDoc.DocumentElement!);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Помилка: " + ex.Message);
+            //}
+        }
+
+        static void DisplayAttribute(string xml)
+        {
+
+            var reader = new XmlTextReader(xml);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                { 
+                    if (reader.Name.Equals("Name"))   
+                    {
+                        Console.WriteLine("{0}", reader.GetAttribute("number"));
+                    }
+                }
+            }
+            //if (node is XmlElement)
+            //{
+            //    if (node.Attributes != null)
+            //    {
+            //        foreach (XmlAttribute attr in node.Attributes)
+            //        {
+            //            Console.WriteLine("  Атрибут: " + attr.Name + " = " + attr.Value);
+            //        }
+            //    }
+            //}
+
+            //foreach (XmlNode childNode in node.ChildNodes)
+            //{
+            //    DisplayAttribute(childNode);
+            //}
+        }
+    }
+}
